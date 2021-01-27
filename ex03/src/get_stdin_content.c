fe8ceef4706d56a29e4878b21126086f2dfd01a0
Mathieu Pointecouteau <mathieu.pointecouteau@epitech.eu>
2021-01-27 17:23:33 +0100

    Fix ex03 Add the null char at the end of buffer

--- ex03/src/get_stdin_content.c
+++ ex03/src/get_stdin_content.c
@@ -9,9 +9,20 @@ char* get_stdin_content(void)
 {
     size_t buffer_size = 1024;
     char* buffer = malloc(sizeof(char) * buffer_size);
+    ssize_t ret;
 
-    if (buffer) {
-        read(STDIN_FILENO, buffer, buffer_size);
+    if (buffer)
+    {
+        ret = read(STDIN_FILENO, buffer, buffer_size);
+        if (ret != -1)
+        {
+            buffer[ret] = '\0';
+        }
+        else
+        {
+            free(buffer);
+            buffer = NULL;
+        }
     }
     return buffer;
 }
